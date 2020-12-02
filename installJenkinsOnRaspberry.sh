c26a90e4af64b274641a0bbb94e02145e2758538
Rodrigo Marques Alves <rodrigo.alves@telefonica.com>
2020-12-02 02:53:19 -0300

    teste

---
+++ installJenkinsOnRaspberry.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+clear
+echo "Install Jenkins on Raspberry"
+
+run_cmd(){
+    sleep 2
+    echo $1
+    if $2; then echo "$1 ok"; else echo "$1 error"; exit 2;fi
+    echo
+}
+
+run_cmd "Deploy openjdk11" "sudo apt install -y openjdk-11-jre"
+run_cmd "Add key" "wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -"
+run_cmd "Add repository" "sudo echo -e 'deb https://pkg.jenkins.io/debian binary/' >> /etc/apt/sources.list.d/jenkins.list"
+run_cmd "Update repositories" "sudo apt-get update"
+run_cmd "Install RPI Monitor" "sudo apt install -y jenkins"
