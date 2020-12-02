a93524642404bef2b7134d9e22c0df77809f3f58
Rodrigo Marques Alves <rodrigo.alves@telefonica.com>
2020-12-02 02:28:56 -0300

    teste

--- getServerInfo.sh
+++ getServerInfo.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 
 echo -e "Ditribuição Linux..:" $(cat /etc/*-release | grep PRETTY | sed s/"PRETTY_NAME="//g)
-echo -e "Sistema Operacional:" 
+
+echo -e """--> \033[1;32mN E T W O R K\e[0m
+\e[100mLocal  IP:\e[49m $(hostname -I | awk '{print $1}')
+\e[100mPublic IP:\e[49m $(curl -q https://ipinfo.io/ip)
+"""
